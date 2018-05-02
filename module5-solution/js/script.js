c06e29c82004287435f1e50021d0eb4c5bf762da
Sharan <amit.sharan@capgemini.com>
2018-05-02 13:13:45 +0530

    module5 - solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -82,8 +82,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-	 // ***** <---- TODO: STEP 1: Substitute [...] ******
-  allCategoriesUrl,buildAndShowHomeHTML,
+  allCategoriesUrl,buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -102,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-
+			console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
